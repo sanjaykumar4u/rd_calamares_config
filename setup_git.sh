7e47f66f9fe6522510706e1e02361b86f772df12
Sanjay Kumar <131436535+Raindeer-OS@users.noreply.github.com>
2023-04-22 14:07:12 +0530

    gpt-improve

--- setup_git.sh
+++ setup_git.sh
@@ -9,19 +9,24 @@ echo "################################################################"
 tput sgr0
 echo
 
-
 project=$(basename `pwd`)
-githubdir="Raindeer-OS"
+username="Raindeer-OS"
 echo "-----------------------------------------------------------------------------"
-echo "this is project https://github.com/$githubdir/$project"
+echo "This is the project: https://github.com/$username/$project"
 echo "-----------------------------------------------------------------------------"
 
 git config --global pull.rebase false
 git config --global push.default simple
 git config --global user.name "Raindeer-OS"
 git config --global user.email "officialraindeeros@gmail.com"
-sudo git config --system core.editor nano
-git remote set-url origin git@github.com:$githubdir/$project
+git config --system core.editor nano
+
+if git remote set-url origin git@github.com:$username/$project; then
+  echo "Git remote URL set to git@github.com:$username/$project"
+else
+  echo "Error: Could not set Git remote URL"
+fi
+
 echo
 tput setaf 3
 echo "################################################################"
