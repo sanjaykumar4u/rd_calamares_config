a3ff098e6bbbfae6f7fe0718425715c4793d88cc
Sanjay Kumar <131436535+Raindeer-OS@users.noreply.github.com>
2023-04-23 12:20:22 +0530

    testing

--- setup_git.sh
+++ setup_git.sh
@@ -9,24 +9,19 @@ echo "################################################################"
 tput sgr0
 echo
 
+
 project=$(basename `pwd`)
-username="Raindeer-OS"
+githubdir="Raindeer-OS"
 echo "-----------------------------------------------------------------------------"
-echo "This is the project: https://github.com/$username/$project"
+echo "this is project https://github.com/$githubdir/$project"
 echo "-----------------------------------------------------------------------------"
 
 git config --global pull.rebase false
 git config --global push.default simple
 git config --global user.name "Raindeer-OS"
 git config --global user.email "officialraindeeros@gmail.com"
-git config --system core.editor nano
-
-if git remote set-url origin git@github.com:$username/$project; then
-  echo "Git remote URL set to git@github.com:$username/$project"
-else
-  echo "Error: Could not set Git remote URL"
-fi
-
+sudo git config --system core.editor nano
+git remote set-url origin git@github.com:$githubdir/$project
 echo
 tput setaf 3
 echo "################################################################"
